7855d801e9e1c0f06026a668b51fab8398402e20
pratikpat81 <106554819+pratikpat81@users.noreply.github.com>
2022-08-22 22:29:06 -0400

    added carousel

---
+++ app.js
@@ -0,0 +1,10 @@
+function onLoad() {
+    console.log("page is loaded")
+
+    var elem = document.querySelector('.target-students');
+    var flkty = new Flickity( elem, {
+      // options
+    });
+
+}
+
