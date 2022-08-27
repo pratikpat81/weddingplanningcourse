f47ac7237d01659f09d683c07976f73f94fb1890
pratikpat81 <106554819+pratikpat81@users.noreply.github.com>
2022-08-27 14:31:43 -0400

    added faq section

--- app.js
+++ app.js
@@ -51,3 +51,32 @@ function updateTestimonial() {
 }
 
 updateTestimonial();
+
+
+//FAQ SECTION
+
+const tablinks = document.getElementsByClassName("tab-link");
+
+const allContent = document.querySelectorAll(".tab-content");
+
+
+for (let i = 0; i < tablinks.length;i++){ 
+    tablinks[i].addEventListener("click", function(e){
+        const current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active","");
+        this.className += " active";
+        
+        //Switch content upon click 
+        const filter = e.target.dataset.filter; 
+        console.log(filter);
+
+        allContent.forEach((content)=>{
+            if(content.classList.contains(filter)){ 
+                content.style.display = "block";
+            } else { 
+                content.style.display = "none";
+            }
+        })
+
+    });
+}
